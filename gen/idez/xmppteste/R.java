b50a4625a957c761040c67b3e13b77e44abd455f
Manoel de Albuquerque Lira Amaro <mano.lira@gmail.com>
2011-10-29 11:17:26 -0300

    vai

--- gen/idez/xmppteste/R.java
+++ gen/idez/xmppteste/R.java
@@ -31,8 +31,9 @@ public final class R {
         public static final int linearLayout1=0x7f070001;
     }
     public static final class layout {
-        public static final int chat=0x7f030000;
-        public static final int main=0x7f030001;
+        public static final int buddies=0x7f030000;
+        public static final int chat=0x7f030001;
+        public static final int main=0x7f030002;
     }
     public static final class menu {
         public static final int chat_menu=0x7f060000;
--- src/idez/xmppteste/XMPPApplication.java
+++ src/idez/xmppteste/XMPPApplication.java
@@ -8,6 +8,7 @@ import org.jivesoftware.smack.ConnectionConfiguration.SecurityMode;
 
 import android.app.Application;
 import android.content.SharedPreferences;
+import android.os.AsyncTask;
 import android.preference.PreferenceManager;
 import android.widget.Toast;
 
@@ -25,25 +26,6 @@ public class XMPPApplication extends Application {
 
 	public void conectar() {
 		
-		if (this.xmppConnection != null)
-			if (this.xmppConnection.isConnected())
-				return;
-		
-		String username = this.prefs.getString("username", "");
-		String password = this.prefs.getString("password", "");
-		String host     = this.prefs.getString("host", "");
-		Integer port    = this.prefs.getInt("port", 0);
-		
-		ConnectionConfiguration config = new ConnectionConfiguration(host, port);
-		config.setSecurityMode(SecurityMode.disabled);
-		config.setSASLAuthenticationEnabled(false);
-		this.xmppConnection = new XMPPConnection(config);
-		try {
-			this.xmppConnection.connect();
-			this.xmppConnection.login(username, password);
-		} catch (Exception e) {
-			Toast.makeText(this, e.getMessage(), Toast.LENGTH_LONG).show();
-		}
 	}
 	
 	public XMPPConnection getXmppConnection() {
@@ -57,5 +39,35 @@ public class XMPPApplication extends Application {
 	}
 	public void setActiveChats(ArrayList<ChatActivity> activeChats) {
 		this.activeChats = activeChats;
+	}
+	
+	class ConectarXmpp extends AsyncTask<String, Integer, String> {
+		
+		@Override
+		protected String doInBackground(String... arg0) {
+			if (xmppConnection != null)
+				if (xmppConnection.isConnected())
+					return "Conexão aberta.";
+			
+			String username = prefs.getString("username", "");
+			String password = prefs.getString("password", "");
+			String host     = prefs.getString("host", "");
+			Integer port    = Integer.parseInt(prefs.getString("port", "5222"));
+			
+			ConnectionConfiguration config = new ConnectionConfiguration(host, port);
+			config.setSecurityMode(SecurityMode.required);
+			config.setSASLAuthenticationEnabled(true);
+			xmppConnection = new XMPPConnection(config);
+			try {
+				xmppConnection.connect();
+				xmppConnection.login(username, password);
+			} catch (Exception e) {
+				Toast.makeText(XMPPApplication.this, e.getMessage(), Toast.LENGTH_LONG).show();
+				return e.getMessage();
+			}
+			
+			return "Conexão realizada com sucesso.";
+		}
+		
 	}
 }
